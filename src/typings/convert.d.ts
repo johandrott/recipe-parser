7623087d8fc35e7e8bcea0de96936936d483a5b8
Johan Drott <johan@halsasomlivsstil.com>
2020-07-16 13:18:25 +0200

    typings

---
+++ src/typings/convert.d.ts
@@ -0,0 +1,5 @@
+export declare function convertFromFraction(value: string): string;
+export declare function getFirstMatch(line: string, regex: RegExp): string;
+export declare function findQuantityAndConvertIfUnicode(
+  ingredientLine: string
+): (string | null)[];
---
+++ src/typings/units.d.ts
@@ -0,0 +1,3 @@
+export declare const units: {
+  [key: string]: string[];
+};
